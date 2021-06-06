624f5759f54152fc23f8090f82c2ec6ed7892e51
wyl <1064902365@qq.com>
2021-06-06 22:00:02 +0800

    完成加固和上传

--- plugin/src/main/groovy/com/enjoy/plugin/JiaguTask.groovy
+++ plugin/src/main/groovy/com/enjoy/plugin/JiaguTask.groovy
@@ -2,7 +2,9 @@ package com.enjoy.plugin
 
 import com.android.builder.model.SigningConfig
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.logging.LogLevel
+import org.gradle.api.tasks.TaskAction
+import org.gradle.process.ExecResult;
 
 class JiaguTask extends DefaultTask {
 
@@ -29,6 +31,16 @@ class JiaguTask extends DefaultTask {
         project.exec {
             it.commandLine("java", "-jar", jiagu.jiaguTools, "-jiagu", apk.absolutePath,
                     apk.parent, "-autosign")
+        }
+
+        def dir = new File(apk.parent)
+        dir.eachFile { file ->
+            if (file.name.endsWith(".apk") && file.name.contains("sign")) {
+              project.exec {
+                    it.commandLine("curl", "-F", "file=@${file.absolutePath}", "-F", "uKey=f2df642bcfc90c79e4464f53db9beb39", "-F", "_api_key=eb6b721fb2aec8d3a41c63eb17b7942c", "https://www.pgyer.com/apiv1/app/upload")
+                }
+            }
+
         }
 
 
